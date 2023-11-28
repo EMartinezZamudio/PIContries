a8944505ee8831a1bb1127e2998da0eb3a018425
Emmanuel <manumtz20@gmail.com>
2023-11-28 14:41:34 -0600

    landing y home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+// modulos
+import { Routes, Route, useLocation } from "react-router-dom";
+
+// componentes
+import Landing from "./views/Landing/Landing";
+
 function App() {
+  // hook de react-ruter
+  const { pathname } = useLocation();
+
+  const body = document.querySelector("body");
+  if (pathname === "/") body.className = "bg-landing";
+  if (pathname === "/home") body.className = "bg-home";
+
   return (
     <div>
-      <h1>Iniciando el PI</h1>
+      <h1>Nav Bar</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </div>
   );
 }
