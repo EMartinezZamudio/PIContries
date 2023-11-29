b11fc4ce1f1c519790749f76f25b92537379b3cf
Emmanuel <manumtz20@gmail.com>
2023-11-29 11:30:14 -0600

    estilos landing

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -2,11 +2,21 @@
 import { Link } from "react-router-dom";
 
 // estilos
-import { landing, btnLanding } from "./Landing.module.css";
+import {
+  landing,
+  btnLanding,
+  tituloLanding,
+  span1,
+  span2,
+} from "./Landing.module.css";
 
 const Landing = () => {
   return (
     <div className={landing}>
+      <p className={tituloLanding}>
+        <span className={span1}>P.I.</span>
+        <span className={span2}>Countries</span>
+      </p>
       <Link to={"/home"}>
         <button className={btnLanding}>Comenzar</button>
       </Link>
