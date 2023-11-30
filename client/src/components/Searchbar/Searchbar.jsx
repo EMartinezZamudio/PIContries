ef5015b73286655b7108409c8bc9bc99bbb5aa9a
Emmanuel <manumtz20@gmail.com>
2023-11-30 16:46:22 -0600

    estilo de search bar

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,20 @@
+// estilos
+import {
+  borderInput,
+  inputSearchbar,
+  btnSearchbar,
+} from "./Searchbar.module.css";
+
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  return (
+    <div className={borderInput}>
+      <ion-icon name="search-sharp"></ion-icon>
+      <input type="text" placeholder="Buscar" className={inputSearchbar} />
+      <button className={btnSearchbar}>
+        <ion-icon name="search-circle"></ion-icon>
+      </button>
+    </div>
+  );
 };
 
 export default Searchbar;
