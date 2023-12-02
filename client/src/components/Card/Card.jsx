f66fad460e015563d9417188bf98009f46c72c27
Emmanuel <manumtz20@gmail.com>
2023-12-02 17:51:03 -0600

    estilos card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
+import { wrapperCard } from "./Card.module.css";
+
 const Card = ({ name, continente, imagen }) => {
   return (
-    <div>
+    <div className={wrapperCard}>
       <h2>{name}</h2>
       <h3>{continente}</h3>
       <img src={imagen} alt="imagen del pais" />
