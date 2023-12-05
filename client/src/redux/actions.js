7fb0fb371b896add1735242e39ce7d3db3b2e534
Emmanuel <manumtz20@gmail.com>
2023-12-05 16:51:34 -0600

    manejo de errores en addCard

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -8,10 +8,16 @@ import URLS from "../helpers/Urls";
 
 export const addCard = (name) => {
   return async (dispatch) => {
-    const { data } = await axios.get(`${URLS.COUNTRIES}?name=${name}`);
-    return dispatch({
-      type: ADD_CARD,
-      payload: data,
-    });
+    try {
+      const { data } = await axios.get(`${URLS.COUNTRIES}?name=${name}`);
+      return dispatch({
+        type: ADD_CARD,
+        payload: data,
+      });
+    } catch (error) {
+      const status = error.response.status;
+      if (status === 404) alert("Pais no encontrado");
+      throw new Error(error.message);
+    }
   };
 };
