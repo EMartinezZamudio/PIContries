f2c993fa6d8cb8b973abe83d0967ff37c981356e
Emmanuel <manumtz20@gmail.com>
2023-12-05 17:15:54 -0600

    manejo de paises duplicados

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -5,6 +5,11 @@ const initialState = {
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case "ADD_CARD":
+      for (let card of state.cards) {
+        if (payload.id === card.id) {
+          return alert("Pais ya agregado");
+        }
+      }
       return {
         ...state,
         cards: [...state.cards, payload],
