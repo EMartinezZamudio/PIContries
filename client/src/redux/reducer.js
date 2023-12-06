70eb36934d45b2e833e4c9c159a37f0528b2c140
Emmanuel <manumtz20@gmail.com>
2023-12-05 20:38:45 -0600

    correcion en actiontypes

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,10 +1,13 @@
+// actionTypes
+import { ADD_CARD, REMOVE_CARD } from "./actionTypes";
+
 const initialState = {
   cards: [],
 };
 
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case "ADD_CARD":
+    case ADD_CARD:
       for (let card of state.cards) {
         if (payload.id === card.id) {
           return alert("Pais ya agregado");
@@ -15,7 +18,7 @@ const rootReducer = (state = initialState, { type, payload }) => {
         cards: [...state.cards, payload],
       };
 
-    case "REMOVE_ADD":
+    case REMOVE_CARD:
       // eslint-disable-next-line no-case-declarations
       const newCards = state.cards.filter((card) => {
         return card.id !== payload;
