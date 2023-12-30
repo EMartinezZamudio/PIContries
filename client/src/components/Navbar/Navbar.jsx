115028b689958aea8422c4d99880ed668793be4b
Emmanuel <manumtz20@gmail.com>
2023-12-29 22:02:52 -0600

    cambios en estilos

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,16 +3,32 @@ import { NavLink } from "react-router-dom";
 import PATHROUTES from "../../helpers/PathRoutes.helpers";
 
 // estilos
-import { navbar, divNavbar } from "./Navbar.module.css";
+import {
+  navbar,
+  divSearchBar,
+  divBtn,
+  btnActive,
+  btnPending,
+} from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={navbar}>
-      <div className={divNavbar}>
-        <span>Countries</span>
-        <NavLink to={PATHROUTES.FORM}>Crear Actividad</NavLink>
+      <div className={divBtn}>
+        <NavLink
+          className={({ isActive }) => (isActive ? btnActive : btnPending)}
+          to={PATHROUTES.HOME}
+        >
+          Inicio
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? btnActive : btnPending)}
+          to={PATHROUTES.FORM}
+        >
+          Actividades
+        </NavLink>
       </div>
-      <div className={divNavbar}>
+      <div className={divSearchBar}>
         <Searchbar />
       </div>
     </nav>
