4163d89056c67eb286b2d27906aefbd8c3ab9e7f
Emmanuel <manumtz20@gmail.com>
2024-01-04 23:37:15 -0600

    variable de entorno en index del server

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const { conn } = require("./src/db.js");
 // controlador para guardar la informacion de la api en la base de datos
 // const saveDataCountries = require("./src/controllers/saveDataCountries.js");
 
-const PORT = 3001;
+const { SV_PORT } = process.env;
 
 conn
   .sync({ force: false })
   .then(() => {
-    server.listen(PORT, async () => {
+    server.listen(SV_PORT, async () => {
       // const response = await saveDataCountries();
       // console.log(response);
 
-      console.log(`Server listening on port ${PORT}`);
+      console.log(`Server listening on port ${SV_PORT}`);
     });
   })
   .catch((error) => console.error(error.message));
